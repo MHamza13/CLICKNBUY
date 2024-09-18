907ee1e2c87b92546f5c5aa50a842ae1efde8347
MHamza13 <iamhamza013@gmail.com>
2024-09-18 17:47:41 +0500

    Your commit message

--- server/Common.js
+++ server/Common.js
@@ -12,9 +12,8 @@ exports.cookieExtractor = function (req) {
   var token = null;
   if (req && req.cookies) {
     token = req.cookies["jwt"];
-    console.log("Extracted JWT Token:", token); // Debugging to see if the token is being extracted
+    console.log("Extracted JWT Token:", token); 
   }
-  token =
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY2ZWE3ZDkxNjIwZTM0MDM4MWNhMTYwZCIsInJvbGUiOiJ1c2VyIiwiaWF0IjoxNzI2NjQzODg0fQ.XUzhqgPmLkbuvTHXU4KX7aeZ9fIwn8eINMZvCQC_ZmM";
+
   return token;
 };
