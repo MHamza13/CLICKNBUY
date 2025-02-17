935366f6a784eeb4bd3751bf460da4214f08eec7
MHamza13 <iamhamza013@gmail.com>
2025-02-17 16:56:51 +0500

    Your commit message

--- server/Common.js
+++ server/Common.js
@@ -2,8 +2,11 @@ const passport = require("passport");
 const nodemailer = require("nodemailer");
 const { google } = require("googleapis");
 
-const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
-const GOOGLE_CLIENT_SECERET = process.env.GOOGLE_CLIENT_SECERET;
+const GOOGLE_CLIENT_ID =
+  process.env.GOOGLE_CLIENT_ID ||
+  "289465848111-fgpo5o38e6acg3gkdeajfvcbp0d9g2ou.apps.googleusercontent.com";
+const GOOGLE_CLIENT_SECERET =
+  process.env.GOOGLE_CLIENT_SECERET || "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";
 
 exports.oauth2Client = new google.auth.OAuth2(
   GOOGLE_CLIENT_ID,
